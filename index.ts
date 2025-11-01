d55a9ce0282ba63070123836b241938a05d0aecd
dumorando <dumorando@proton.me>
2025-11-01 15:12:50 -0400

    forgot to remove cors

--- index.ts
+++ index.ts
@@ -2,10 +2,12 @@ import Elysia from "elysia";
 import GetModels from "./misc/GetModels";
 import ChatCompletions from "./misc/ChatCompletions";
 import getPort from "./misc/GetPort";
+import cors from "@elysiajs/cors";
 
 const PORT = getPort();
 
 new Elysia()
+    .use(cors())
     .get('/v1beta/models', async () => {
         return await GetModels();
     })
