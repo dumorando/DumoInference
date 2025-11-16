8b87d90c83ebefafb9107188454f5990bb9c2ae3
dumorando <144301348+dumorando@users.noreply.github.com>
2025-11-16 17:00:06 -0500

    holy shit

--- src/v1beta.ts
+++ src/v1beta.ts
@@ -2,7 +2,7 @@ import Elysia from "elysia";
 import { GetModelsOpenAI } from "../misc/GetModels";
 import ChatCompletions from "../misc/ChatCompletions";
 
-export default new Elysia({ prefix: '/v1beta' })
+export default new Elysia()
     .get('/v1beta/models', async () => {
         return await GetModelsOpenAI();
     })
